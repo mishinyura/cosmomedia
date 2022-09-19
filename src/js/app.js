85161a21fbcb23e2c2e99580aa80f04f305f49db
Yura Mishin <robothot@yandex.ru>
2022-09-19 21:53:48 +0300

    fix: пофиксил баги

--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,61 @@ import './modules/mouse.js'
 import './modules/menu.js'
 import './modules/burger.js'
 import './modules/hero.js'
+import './modules/animation.js'
 import './modules/swiper-bundle.min.js'
 
 flsFunctions.isWebp();
 
+
+
+
+
+//progress Bar and header
+
+let header = document.querySelector('.header'),
+  fullHeight = document.body.scrollHeight,
+  innerPage = window.innerHeight,
+  progressBar = document.querySelector('.footer__ellipse'),
+  progressBtn = document.querySelector('.footer__btn-progress');
+
+
+function fillProgress() {
+  fullHeight = document.body.scrollHeight
+  innerPage = window.innerHeight
+  progressBar.style.strokeDashoffset = 270 - (scrollY * 270 / (fullHeight - innerPage)) + '%'
+}
+
+window.addEventListener('scroll', fillProgress)
+window.addEventListener('resize', fillProgress)
+
+
+window.addEventListener('scroll', () => {
+  if (scrollY >= 200) {
+    header.style.backgroundColor = 'black'
+    progressBtn.classList.add('progress-active')
+  } else {
+    document.querySelector('.header').style.backgroundColor = 'transparent'
+    progressBtn.classList.remove('progress-active')
+  }
+})
+
+progressBtn.addEventListener('click', () => {
+  document.querySelector('.hero').scrollIntoView({
+    block: 'nearest',
+    behavior: 'smooth'
+  });
+})
+
+
+
+
+
+
+
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
-  // spaceBetween: 100,
+  spaceBetween: 30,
   centeredSlides: true,
   parallax: true,
   autoplay: {
@@ -27,28 +73,45 @@ const swiper = new Swiper('.swiper', {
     }
   },
 
-  // // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
 });
 
-const slider = new Swiper('.slider', {
+
+
+
+const revSlider = new Swiper('.reviews__wrapper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-  // spaceBetween: 100,
+  spaceBetween: 30,
+  centeredSlides: true,
+  parallax: true,
+  autoplay: {
+    delay: 2500,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+
+    1030: {
+      slidesPerView: 1.5,
+    }
+  },
+});
+
+
+
+
+const fslider = new Swiper('.first_slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 10,
   centeredSlides: true,
   parallax: true,
   slidesPerView: 1,
@@ -72,30 +135,23 @@ const slider = new Swiper('.slider', {
 });
 
 
-const revSlider = new Swiper('.reviews__wrapper', {
+
+const sslider = new Swiper('.second_slider', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-  // spaceBetween: 100,
+  spaceBetween: 10,
   centeredSlides: true,
   parallax: true,
-  autoplay: {
-    delay: 2500,
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
+  slidesPerView: 1,
+  centeredSlides: true,
+  inverse: true,
 
-    1030: {
-      slidesPerView: 1.5,
-    }
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: fraction,
   },
-
-  // // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
 
   // Navigation arrows
   navigation: {
@@ -108,3 +164,7 @@ const revSlider = new Swiper('.reviews__wrapper', {
   //   el: '.swiper-scrollbar',
   // },
 });
+
+fslider.controller.control = sslider
+sslider.controller.control = fslider
+// sslider.changeLanguageDirection(rtl)
--- src/js/modules/mouse.js
+++ src/js/modules/mouse.js
@@ -1,35 +1,29 @@
-// const mouseBorder = document.querySelector('.mouse-border'),
-//       mouseCenter = document.querySelector('.mouse-center');
-      
-
-// window.addEventListener('mousemove', (event) => {
-//   // console.log(event.pageX, event.pageY)
-//   console.log(event.offsetX, event.offsetY)
-//   mouseBorder.style.left = event.pageX - 10 + 'px'
-//   mouseBorder.style.top = event.pageY - 10 + 'px'
-//   mouseCenter.style.left = event.pageX - 2 + 'px'
-//   mouseCenter.style.top = event.pageY - 2 + 'px'
-// })
-
 document.addEventListener('DOMContentLoaded', () => {
-
   const followCursor = () => { // объявляем функцию followCursor
-    const mouseBorde = document.querySelector('.mouse-border') // ищем элемент, который будет следовать за курсором
+    const mouseCenter = document.querySelector('.mouse-center')
+    const mouseBorder = document.querySelector('.mouse-border') // ищем элемент, который будет следовать за курсором
 
     window.addEventListener('mousemove', e => { // при движении курсора
       // console.log(e.pageX, e.pageY)
-      console.log(window.pageYOffset, window.pageXOffset)
       const target = e.target // определяем, где находится курсор
       if (!target) return
 
       if (target.closest('a') || target.closest('button')) { // если курсор наведён на ссылку
-        mouseBorde.classList.add('cursor-active') // элементу добавляем активный класс
+        mouseBorder.classList.add('cursor-active') // элементу добавляем активный класс
       } else { // иначе
-        mouseBorde.classList.remove('cursor-active') // удаляем активный класс
+        mouseBorder.classList.remove('cursor-active') // удаляем активный класс
       }
 
-      mouseBorde.style.left = e.pageX + 'px' // задаём элементу позиционирование слева
-      mouseBorde.style.top = e.pageY + 'px' // задаём элементу позиционирование сверху
+      mouseCenter.style.left = e.pageX + 'px' // задаём элементу позиционирование слева
+      mouseCenter.style.top = e.pageY + 'px'
+
+      setTimeout(() => {
+        mouseBorder.style.left = e.pageX + 'px' // задаём элементу позиционирование слева
+        mouseBorder.style.top = e.pageY + 'px' // задаём элементу позиционирование сверху
+      }, 50)
+
+      
+      
     })
   }
 
